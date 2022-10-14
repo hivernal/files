fa0d16de3fb43a51e450e337be4be0d4cafa764e
hivernal <geryaevna@mail.ru>
2022-10-15 02:24:02 +0700

    change startup script

--- config_qtile.py
+++ config_qtile.py
@@ -1,6 +1,8 @@
-from libqtile import bar, layout, extension
-from libqtile.config import Click, Drag, Group, Key, KeyChord, Match, Screen
+from libqtile import bar, layout , widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
+from libqtile.widget.battery import Battery, BatteryState
+#from libqtile.utils import guess_terminal
 
 import os
 import subprocess
@@ -14,75 +16,60 @@ def autostart():
     subprocess.run([home])
 
 
+class MyBattery(Battery):
+    def build_string(self, status):
+        if status.state == BatteryState.DISCHARGING:
+            if status.percent > 0.75:
+                char = ''
+            elif status.percent > 0.45:
+                char = ''
+            else:
+                char = ''
+        elif status.percent >= 1 or status.state == BatteryState.FULL:
+            char = ''
+        elif status.state == BatteryState.EMPTY or \
+                (status.state == BatteryState.UNKNOWN and status.percent == 0):
+            char = ''
+        else:
+            char = ''
+        return self.format.format(char=char, percent=status.percent)
+battery = MyBattery(
+    format='{char}',
+    low_foreground="#282c34",
+    show_short_text=False,
+    low_percentage=0.12,
+    foreground="#fcfcfc",
+    notify_below=12,
+)
+
 mod = "mod4"
+terminal = "alacritty"
 
 keys = [
-    # Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    # Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     Key([mod], "Return", lazy.layout.swap_left()),
-    Key([mod], "h", lazy.layout.shrink_main(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.grow_main(), desc="Move focus to right"),
+    Key([mod], "h", lazy.layout.shrink_main(), desc="shrink master area"),
+    Key([mod], "l", lazy.layout.grow_main(), desc="grow master area"),
+
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
-    # KeyChord([mod], "r", [
-    #     Key([], "h", lazy.layout.grow_left()),
-    #     Key([], "l", lazy.layout.grow_right()),
-    #     Key([], "j", lazy.layout.grow_down()),
-    #     Key([], "k", lazy.layout.grow_up()),
-    #     Key([], "r", lazy.ungrab_chord()()),
-    #     Key([mod], "r", lazy.ungrab_chord()()),
-    #     Key([mod], "h", lazy.layout.grow_left()),
-    #     Key([mod], "l", lazy.layout.grow_right()),
-    #     Key([mod], "j", lazy.layout.grow_down()),
-    #     Key([mod], "k", lazy.layout.grow_up())],
-    #     mode = "Resize"),
-    #
-    # KeyChord([mod], "w", [
-    #     Key([], "h", lazy.layout.shuffle_left()),
-    #     Key([], "l", lazy.layout.shuffle_right()),
-    #     Key([], "j", lazy.layout.shuffle_down()),
-    #     Key([], "k", lazy.layout.shuffle_up()),
-    #     Key([], "w", lazy.ungrab_chord()()),
-    #     Key([mod], "w", lazy.ungrab_chord()()),
-    #     Key([mod], "h", lazy.layout.shuffle_left()),
-    #     Key([mod], "l", lazy.layout.shuffle_right()),
-    #     Key([mod], "j", lazy.layout.shuffle_down()),
-    #     Key([mod], "k", lazy.layout.shuffle_up()),
-    #     Key([mod], "w", lazy.ungrab_chord()())],
-    #     mode = "Move windows"),
-
-    Key([mod, "shift"], "Return", lazy.spawn("alacritty"), desc="Launch terminal"),
-    Key([mod, "shift"], "q", lazy.spawn("shutdown -h now"), desc="Launch terminal"),
+    Key([mod, "shift"], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "shift"], "e", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "p", lazy.spawn("dmenu_run -nf '#fcfcfc' -nb '#282c34' -sb '#646870' -sf '#fcfcfc' -fn 'JetBrainsMonoMedium Nerd Font-13'"), desc="dmenu"),
+    Key([mod, "shift"], "q", lazy.spawn("shutdown -h now"), desc="poweroff"),
     Key([mod], "f", lazy.window.toggle_floating(), desc="from/to floating"),
 
-    Key(['mod4'], 'p', lazy.run_extension(extension.DmenuRun(
-        dmenu_bottom = True,
-        dmenu_font="JetBrainsMonoMedium Nerd Font-13",
-        background="#282c34",
-        foreground="#fcfcfc",
-        selected_background="#5c6667",
-        selected_foreground="#fcfcfc",
-        dmenu_ignorecase = True,
-        dmenu_prompt = "dmenu",
-    ))),
-    # Key([],"XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"), desc="Volume"),
-    # Key([],"XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"), desc="Volume"),
-    # Key([],"XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle"), desc="Volume"),
-    # Key([],"XF86AudioMicMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle"), desc="Volume"),
-    Key([],"XF86AudioRaiseVolume", lazy.spawn("amixer set Master 5%+"), desc="Volume"),
-    Key([],"XF86AudioLowerVolume", lazy.spawn("amixer set Master 5%-"), desc="Volume"),
-    Key([],"XF86AudioMute", lazy.spawn("amixer set Master toggle"), desc="Volume"),
-    Key([],"XF86AudioMicMute", lazy.spawn("amixer set Master toggle"), desc="Volume"),
+    Key([],"XF86AudioRaiseVolume", lazy.spawn("pamixer -i 5"), desc="Volume"),
+    Key([],"XF86AudioLowerVolume", lazy.spawn("pamixer -d 5"), desc="Volume"),
+    Key([],"XF86AudioMute", lazy.spawn("pamixer -t"), desc="Volume"),
+    # Key([],"XF86AudioMicMute", lazy.spawn("pamixer -t"), desc="Volume"),
     Key([],"XF86MonBrightnessUp", lazy.spawn("xbacklight -inc 5"), desc="Brightness"),
     Key([],"XF86MonBrightnessDown", lazy.spawn("xbacklight -dec 5"), desc="Brightness"),
 ]
@@ -100,16 +87,14 @@ for i in groups:
                 desc="Switch to group {}".format(i.name),
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
+            # Key(
+            #     [mod, "shift"],
+            #     i.name,
+            #     lazy.window.togroup(i.name, switch_group=True),
+            #     desc="Switch to & move focused window to group {}".format(i.name),
+            # ),
+            Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+                desc="move focused window to group {}".format(i.name)),
         ]
     )
 
@@ -119,31 +104,26 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="JetBrainsMono NF",
+    font="JetBrainsMonoMedium Nerd Font",
     fontsize=16,
-    padding=3,
+    padding=5,
 )
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        # wallpaper = '/home/nikita/.local/share/backgrounds/wave-dark-arch.png',
-        # wallpaper_mode = 'fill',
-        top = bar.Gap(34),
+        top = bar.Gap(40),
         # top=bar.Bar(
         #     [
-        #         widget.CurrentLayout(),
         #         widget.GroupBox(),
-        #         widget.Prompt(),
         #         widget.WindowName(),
-        #         widget.BatteryIcon(),
-        #         widget.Battery(update_interval = 2, font = 'JetBrainsMono NF',
-        #                         unknown_char='', charge_char = '', 
-        #                         discharge_char = '',
-        #                         format = '{char} {percent:2.0%}'),
-        #         widget.Clock(format="%H:%M:%S"),
+        #         widget.Battery(format='[ BAT {percent:2.0%} ]'),
+        #         # widget.Wlan(interface='wlo1',format="[ NET {essid} ]" ),
+        #         widget.PulseVolume(format="[ NET {essid} ]" ),
+        #         widget.Clock(format="[ %H:%M:%S ]"),
+        #         # widget.QuickExit(),
         #     ],
-        #     30,
+        #     35,
         #     # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
         #     # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         # ),
@@ -163,6 +143,7 @@ follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(
+    border_width = 0, # was add
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
@@ -172,8 +153,7 @@ floating_layout = layout.Floating(
         Match(wm_class="ssh-askpass"),  # ssh-askpass
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
-    ],
-    border_width = 0
+    ]
 )
 auto_fullscreen = True
 focus_on_window_activation = "smart"
--- startup.sh
+++ startup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # after installation
-sudo pacman -S intel-ucode xorg xorg-server xorg-apps xf86-video-intel xf86-input-libinput mesa mesa-utils xorg-xinit intel-media-driver libva-utils libva-vdpau-driver libva-intel-driver libvdpau-va-gl vdpauinfo intel-gpu-tools --needed && sudo grub-mkconfig -o /boot/grub/grub.cfg
+# sudo pacman -S intel-ucode xorg xorg-server xorg-apps xf86-video-intel xf86-input-libinput mesa mesa-utils xorg-xinit intel-media-driver libva-utils libva-vdpau-driver libva-intel-driver libvdpau-va-gl vdpauinfo intel-gpu-tools --needed && sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # general packages
-sudo pacman -S firefox firefox-i18n-ru firefox-i18n-en-us firefox-spell-ru telegram-desktop gcc gdb nodejs neovim npm python-pip jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src libreoffice-still libreoffice-still-ru hunspell hunspell-ru hunspell-en_us thunderbird thunderbird-i18n-ru thunderbird-i18n-en-us vlc rsync git gtk-engine-murrine sassc transmission-gtk gthumb gparted cmake lsd unzip wget xreader --needed
-python3 -m pip install --user --upgrade pynvim
+# sudo pacman -S firefox firefox-i18n-ru firefox-i18n-en-us firefox-spell-ru telegram-desktop gcc nodejs neovim npm python-pip jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-src libreoffice-still libreoffice-still-ru hunspell hunspell-ru hunspell-en_us thunderbird thunderbird-i18n-ru thunderbird-i18n-en-us vlc rsync git gtk-engine-murrine sassc transmission-gtk gthumb gparted cmake lsd unzip wget xreader unrar curl bash-completion --needed
+# python3 -m pip install --user --upgrade pynvim
+
+# lsp and dap
+# sudo pacman -S lldb clang gdb bash-language-server --needed
 
 # packages for WM
-sudo pacman -S flameshot feh lxsession-gtk3 xorg-xbacklight qt5ct alacritty xmonad xmonad-contrib xmobar breeze dmenu lxappearance --needed
+# sudo pacman -S flameshot feh lxsession-gtk3 xorg-xbacklight qt5ct alacritty xmonad xmonad-contrib xmobar breeze dmenu lxappearance --needed
 
 # alsa
 #sudo pacman -S alsa-utils alsa-tools --needed
@@ -16,22 +19,21 @@ sudo pacman -S flameshot feh lxsession-gtk3 xorg-xbacklight qt5ct alacritty xmon
 # pipewire
 # sudo pacman -S pipewire pipewire-jack pipewire-alsa pipewire-pulse --needed
 
+# pulseaudio
+# sudo pacman -S pulseaudio pulseaudio-bluetooth pulseaudio-alsa pulseaudio-jack pamixer pavucontrol --needed
+
 # bluetooth
 #sudo pacman -S bluez bluez-utils blueman --needed && sudo systemctl enable bluetooth
 
 # delete packages from gnome de
 # sudo pacman -Rsn totem sushi simple-scan nano gnome-weather gnome-photos gnome-maps gnome-contacts gnome-clocks gnome-calendar gnome-boxes gnome-books eog epiphany cheese --needed
 
-#sudo pacman -Scc
-
-
 # themes
-#cd ~ && git clone https://github.com/vinceliuice/Qogir-theme.git && cd Qogir-theme && ./install.sh
-#cd ~ && git clone https://github.com/vinceliuice/Graphite-gtk-theme.git && cd Graphite-gtk-theme && ./install.sh -t blue --tweaks rimless normal && cd wallpaper && ./install-wallpapers.sh
-#cd ~ && git clone https://github.com/vinceliuice/Tela-icon-theme.git && cd Tela-icon-theme && ./install.sh
-#cd ~ && git clone https://github.com/vinceliuice/Orchis-theme.git && cd Orchis-theme && ./install.sh --tweaks solid
-#cd ~ && rm -rf Qogir-theme Graphite-gtk-theme Tela-icon-theme Orchis-theme
-
+cd ~ && git clone https://github.com/vinceliuice/Qogir-theme.git && cd Qogir-theme && ./install.sh
+cd ~ && git clone https://github.com/vinceliuice/Graphite-gtk-theme.git && cd Graphite-gtk-theme && ./install.sh -t blue --tweaks rimless normal && cd wallpaper && ./install-wallpapers.sh
+cd ~ && git clone https://github.com/vinceliuice/Tela-icon-theme.git && cd Tela-icon-theme && ./install.sh
+cd ~ && git clone https://github.com/vinceliuice/Orchis-theme.git && cd Orchis-theme && ./install.sh --tweaks solid
+cd ~ && rm -rf Qogir-theme Graphite-gtk-theme Tela-icon-theme Orchis-theme
 
 # JetBrainsMono font
 #wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip &&
